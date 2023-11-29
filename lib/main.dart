0dd76e36f7db5ce8068a4ad3ab34be92ce2701b2
dongeranguk <rooetym@naver.com>
2023-11-29 23:17:52 +0900

    #1 - 개발환경 세팅
    개발환경을 초기화하였다.

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TtossApp());
+}
+
+class TtossApp extends StatefulWidget {
+
+  const TtossApp({super.key});
+
+  @override
+  State<TtossApp> createState() => _TtossAppState();
+}
+
+class _TtossAppState extends State<TtossApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
